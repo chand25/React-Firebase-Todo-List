ba8b26a8c67bb5810e6771875ad404de2bb84d87
chand25 <moore.shantel@gmail.com>
2017-01-12 16:58:37 -0500

    Delete key.js

--- src/key.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyD5sOb1c66cCWfTg9X1oVXgaP-UK2Ac1mU",
-    authDomain: "mytodo-95fee.firebaseapp.com",
-    databaseURL: "https://mytodo-95fee.firebaseio.com",
-    storageBucket: "mytodo-95fee.appspot.com",
-    messagingSenderId: "1041616237087"
-  };
-  firebase.initializeApp(config);
